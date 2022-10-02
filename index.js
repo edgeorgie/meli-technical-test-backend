559a44f402bd779cf2188fea6d26ed9b2a850110
Edwin Jorge <ed.jorge1122@gmail.com>
2022-10-01 22:00:46 -0500

    ADD: index structure refactored -> app.js

--- index.js
+++ index.js
@@ -1,14 +1,5 @@
-const express = require('express')
-const cors = require('cors')
-
-const routes = require('./routes/index.routes')
-
-const app = express()
-
+const app = require('./app')
 const PORT = 3001 || 5000
-
-app.use(cors())
-app.use('/api/items', routes)
 
 app.listen(PORT, () => {
   console.log('Listening on port', PORT)
