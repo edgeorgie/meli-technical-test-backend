41e47799e9267d7806a7f41d1adf0439bb089449
Edwin Jorge <ed.jorge1122@gmail.com>
2022-10-01 19:39:10 -0500

    ADD: routes structure | EDIT: routes added to index main route

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require('express')
 const cors = require('cors')
 
+const routes = require('./routes/index.routes')
+
 const app = express()
 
 const PORT = 3001 || 5000
 
-app.use(cors());
+app.use(cors())
+app.use('/api/items', routes)
 
 app.listen(PORT, () => {
   console.log('Listening on port', PORT)
