614759cae3cc932981c2d2a5fab616c608ac3c39
Edwin Jorge <ed.jorge1122@gmail.com>
2022-10-03 13:27:47 -0500

    EDIT: app.js route remove

--- app.js
+++ app.js
@@ -6,9 +6,6 @@ const routes = require('./routes/index.routes')
 const app = express()
 
 app.use(cors())
-app.use((req, res) => {
-  res.send('Backend desplegado para producción: PRUEBA TÉCNICA (FRONT-END DEVELOPER) DE EDWIN JORGE PARA MELI.')
-})
 app.use('/api/items', routes)
 
 module.exports = app
